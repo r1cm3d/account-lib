9d69aa29e93d70e6d1c73e1a1ade55fa4a6ecf95
r1cm3d <ricardo.medeiros@pismo.io>
2021-08-03 13:22:26 -0300

    Add integration test harness
    This patch adds integration test harness providing a way to create
    integration tests easily.
    
    Closes #13
    
    Signed-off-by: r1cm3d <ricardo.medeiros@pismo.io>

--- acc.go
+++ acc.go
@@ -7,52 +7,46 @@ import (
 	"net/http"
 )
 
-type payload struct {
-	Data *account `json:"data"`
-}
+type (
+	payload struct {
+		Data *account `json:"data"`
+	}
 
-// Account represents an account in the form3 org section.
-// See https://api-docs.form3.tech/api.html#organisation-accounts for
-// more information about fields.
-type account struct {
-	Attributes     *attributes `json:"attributes,omitempty"`
-	ID             string      `json:"id,omitempty"`
-	OrganisationID string      `json:"organisation_id,omitempty"`
-	Type           string      `json:"type,omitempty"`
-	Version        *int64      `json:"version,omitempty"`
-}
+	account struct {
+		Attributes     *attributes `json:"attributes,omitempty"`
+		ID             string      `json:"id,omitempty"`
+		OrganisationID string      `json:"organisation_id,omitempty"`
+		Type           string      `json:"type,omitempty"`
+		Version        *int64      `json:"version,omitempty"`
+	}
 
-// Attributes represents account attributes in the form3 org section.
-// See https://api-docs.form3.tech/api.html#organisation-accounts for
-// more information about fields.
-type attributes struct {
-	Classification          *string  `json:"account_classification,omitempty"`
-	MatchingOptOut          *bool    `json:"account_matching_opt_out,omitempty"`
-	Number                  string   `json:"account_number,omitempty"`
-	AlternativeNames        []string `json:"alternative_names,omitempty"`
-	BankID                  string   `json:"bank_id,omitempty"`
-	BankIDCode              string   `json:"bank_id_code,omitempty"`
-	BaseCurrency            string   `json:"base_currency,omitempty"`
-	Bic                     string   `json:"bic,omitempty"`
-	Country                 *string  `json:"country,omitempty"`
-	Iban                    string   `json:"iban,omitempty"`
-	JointAccount            *bool    `json:"joint_account,omitempty"`
-	Name                    []string `json:"name,omitempty"`
-	SecondaryIdentification string   `json:"secondary_identification,omitempty"`
-	Status                  *string  `json:"status,omitempty"`
-	Switched                *bool    `json:"switched,omitempty"`
-}
+	attributes struct {
+		Classification          *string  `json:"account_classification,omitempty"`
+		MatchingOptOut          *bool    `json:"account_matching_opt_out,omitempty"`
+		Number                  string   `json:"account_number,omitempty"`
+		AlternativeNames        []string `json:"alternative_names,omitempty"`
+		BankID                  string   `json:"bank_id,omitempty"`
+		BankIDCode              string   `json:"bank_id_code,omitempty"`
+		BaseCurrency            string   `json:"base_currency,omitempty"`
+		Bic                     string   `json:"bic,omitempty"`
+		Country                 *string  `json:"country,omitempty"`
+		Iban                    string   `json:"iban,omitempty"`
+		JointAccount            *bool    `json:"joint_account,omitempty"`
+		Name                    []string `json:"name,omitempty"`
+		SecondaryIdentification string   `json:"secondary_identification,omitempty"`
+		Status                  *string  `json:"status,omitempty"`
+		Switched                *bool    `json:"switched,omitempty"`
+	}
+)
 
-// FIXME: error 500
 func create(acc account) error {
 	data, err := json.Marshal(payload{Data: &acc})
-	s := string(data)
-	fmt.Println(s)
 
 	if err != nil {
 		return err
 	}
 
+	// TODO: change to use configuration
 	resp, err := http.Post("http://0.0.0.0:8080/v1/organisation/accounts", "application/json", bytes.NewBuffer(data))
 
 	if err != nil {
@@ -61,6 +55,7 @@ func create(acc account) error {
 
 	var ret account
 
+	// TODO: Implement the unmarshalling logic here
 	json.NewDecoder(resp.Body).Decode(&ret)
 
 	fmt.Println(ret)
--- acc_test.go
+++ acc_test.go
@@ -1,28 +1,28 @@
 package acc
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+)
+
+var (
+	_fakeStubID = "ad27e265-9605-4b4b-a0e5-3003ea9cc4d2"
+	_itAddress = "0.0.0.0"
+	_itPort = "8080"
+)
 
 func TestCreateIntegration(t *testing.T) {
 	skipShort(t)
-	// TODO: Prepare table here
-	country := "GB"
+	deleteStub(t)
 	// TODO: test passing all arguments
 
 	// WITH CoP
 	// WITHOUT CoP
-	account := account{
-		Attributes: &attributes{
-			BankID:                 "400300",
-			BankIDCode:             "GBDSC",
-			BaseCurrency:           "GBP",
-			Bic:                    "NWBKGB22",
-			Country:                &country,
-			Name:                   []string{"BRUCE", "WAYNE"},
-		},
-		ID:             "ad27e265-9605-4b4b-a0e5-3003ea9cc4d2",
-		OrganisationID: "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
-		Type:           "accounts",
-	}
+	account := stubAccount()
 
 	if err := create(account); err != nil {
 		t.Fail()
@@ -38,9 +38,75 @@ func TestHealth(t *testing.T) {
 	}
 }
 
-
 func skipShort(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping integration test")
+	}
+}
+
+func stubAccount() account {
+	country := "GB"
+
+	return account{
+		Attributes: &attributes{
+			BankID:       "400300",
+			BankIDCode:   "GBDSC",
+			BaseCurrency: "GBP",
+			Bic:          "NWBKGB22",
+			Country:      &country,
+			Name:         []string{"BRUCE", "WAYNE"},
+		},
+		ID:             _fakeStubID,
+		OrganisationID: "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
+		Type:           "accounts",
+	}
+}
+
+func deleteStub(t *testing.T) {
+	const (
+		success = 204
+		notFound = 404
+	)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("http://%s:%s/v1/organisation/accounts/%s?version=0", _itAddress, _itPort, _fakeStubID), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if (resp.StatusCode != success) && (resp.StatusCode != notFound) {
+		t.Fatalf("Error on delete %s", _fakeStubID)
+	}
+
+	fmt.Println(resp)
+	fmt.Println(resp.Status)
+	fmt.Println(resp.StatusCode)
+}
+
+func addStub(t *testing.T) {
+	deleteStub(t)
+	const success = 201
+
+	stub := stubAccount()
+	data, err := json.Marshal(payload{Data: &stub})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := http.Post(fmt.Sprintf("http://%s:%s/v1/organisation/accounts", _itAddress, _itPort), "application/json", bytes.NewBuffer(data))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != success {
+		t.Fatal("Error on addStub")
 	}
 }
